97b742853d7db3a0a27a3dc2a26b7187a5e219a7
baranikk <141861263+baranikk@users.noreply.github.com>
2023-08-11 23:16:42 +0530

    Add files via upload

---
+++ day2/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class Mobile {
+	 public static void main(String[] args) {
+	      
+	       
+	        int battery=80;
+	        int price = 15000;
+	        long mobNum=897654321;	     
+	        String mobModel = "SAMSUNG";
+	        boolean android = true;       
+	       
+	        System.out.println(mobModel);
+	        System.out.println(price);
+	        System.out.println(battery);
+	        System.out.println(mobNum);
+	        System.out.println(android);
+	        
+	        
+	        
+	    }
+	}
+
+
+
---
+++ day2/NegativetoPositiveConv.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class NegativetoPositiveConv {
+	public static void main(String[] args)
+	{
+int n=-40;
+		
+		if(n<0)
+		
+		{
+			n=(-n);
+			System.out.println(n);
+		}
+		
+		
+	}
+
+}
